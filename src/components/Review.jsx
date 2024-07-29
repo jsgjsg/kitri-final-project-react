c8f567b0d82cdbdc635e428de77dbad22caa82bc
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-29 15:04:06 +0900

    Feat: "후기 페이지 뼈대 구현"
    후기 페이지 뼈대 구현 완료

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,11 @@
+import ReviewFilter from "./ReviewFilter";
 import ReviewList from "./ReviewList";
 
 const Review = () => {
   return (
     <div className="p-4 border border-gray-300 rounded-md">
       Review 페이지
+      <ReviewFilter />
       <ReviewList />
     </div>
   );
