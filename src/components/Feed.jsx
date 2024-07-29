2ea57642d27097258a99a6b2e95ecb39c5f5398a
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-29 17:14:26 +0900

    Feat: "피드 데이터 Spring에서 가져오기 구현"

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,27 @@
+import axios from "axios";
+import FeedFilter from "./FeedFilter";
+import FeedList from "./FeedList";
+import { useEffect, useState } from "react";
+
 const Feed = () => {
-  
+  const [feeds, setFeeds] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8080/api/feeds")
+      .then(response => {
+        setFeeds(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error("Error: ", error);
+      });
+  }, []);
+
   return (
-    <div>
+    <div className="p-4 border border-gray-300 rounded-md">
       Feed 페이지
+      <FeedFilter />
+      <FeedList feeds = {feeds} />
     </div>
   );
 }
