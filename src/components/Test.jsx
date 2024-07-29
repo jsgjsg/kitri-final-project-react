af4870462f0ac5a65b0009e32d4c7511f666d1d1
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-29 10:37:47 +0900

    Feat: "데이터베이스와 연결"
    Spring을 통해 db의 데이터 가져오기 테스트

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,30 +1,34 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Test = () => {
-
-  const [test, setTest] = new useState(null);
+  const [test, setTest] = useState(null);
 
   useEffect(() => {
     axios.get("http://127.0.0.1:8080/test")
-    .then(response => {
-      console.log(response);
-      console.log("Data: ", response.data);
-      setTest(response.data);
-    })
-    .catch(error => {
-      console.error("Error: ", error);
-    });
+      .then(response => {
+        console.log(response);
+        console.log("Data: ", response.data);
+        setTest(response.data[0]);
+      })
+      .catch(error => {
+        console.error("Error: ", error);
+      });
   }, []);
-  
   
   return (
     <div>
       <div>테스트~</div>
-      <div>{test}</div>
+      {test && (
+        <div>
+          <div>id: {test.id}</div>
+          <div>testcol1: {test.testcol1}</div>
+          <div>testcol2: {test.testcol2}</div>
+          <div>testcol3: {test.testcol3}</div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Test;
