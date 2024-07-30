9d15832a773f93cf78d0bcd1b747d8b694ec9bdf
psy <po2260793@gmail.com>
2024-07-30 16:02:34 +0900

    Style: "prettier로 인한 변경 수정"

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,8 @@ import MyPage from "./components/MyPage";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReviewForm from "./components/ReviewForm";
 import FeedForm from "./components/FeedForm";
-<<<<<<< HEAD
-import Comments from "./components/FeedComment"; // 댓글 페이지 컴포넌트
-=======
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
->>>>>>> main
 
 function App() {
   return (
@@ -21,13 +17,62 @@ function App() {
       <div className="flex flex-col min-h-screen">
         <nav className="bg-gray-800 text-white fixed w-full top-0 left-0 z-10 shadow-md">
           <ul className="flex justify-around p-4 space-x-6">
-            <li><Link to="/dashboard" className="hover:text-yellow-300 transition-colors">Home</Link></li>
-            <li><Link to="/test" className="hover:text-yellow-300 transition-colors">Test</Link></li>
-            <li><Link to="/feed" className="hover:text-yellow-300 transition-colors">Feed</Link></li>
-            <li><Link to="/navigation" className="hover:text-yellow-300 transition-colors">Navigation</Link></li>
-            <li><Link to="/review" className="hover:text-yellow-300 transition-colors">Review</Link></li>
-            <li><Link to="/qna" className="hover:text-yellow-300 transition-colors">Dr.QnA</Link></li>
-            <li><Link to="/myPage" className="hover:text-yellow-300 transition-colors">MyPage</Link></li>
+            <li>
+              <Link
+                to="/dashboard"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/test"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                Test
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/feed"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                Feed
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/navigation"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                Navigation
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/review"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                Review
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/qna"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                Dr.QnA
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/myPage"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                MyPage
+              </Link>
+            </li>
           </ul>
         </nav>
 
@@ -38,7 +83,6 @@ function App() {
             <Route path="/test" element={<Test />} />
             <Route path="/feed" element={<Feed />} />
             <Route path="/feed/form" element={<FeedForm />} />
-            <Route path="/comments" element={<Comments />} />
             <Route path="/navigation" element={<Navigation />} />
             <Route path="/review" element={<Review />} />
             <Route path="/review/form" element={<ReviewForm />} />
