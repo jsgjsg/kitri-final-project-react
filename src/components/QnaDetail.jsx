f46ba5bf911018e8e563d36fc2449f54d2b4512d
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-31 10:49:42 +0900

    Feat: "Qna 페이지 뼈대 구현"
    상세 보기 제외한 나머지 부분 구현

---
+++ src/components/QnaDetail.jsx
@@ -0,0 +1,14 @@
+const QnaDetail = () => {
+
+  // Question, Answer 데이터 받기
+  // 한번에 다룰수 있는가?
+  // 날짜 순으로 정렬하기!
+
+  return (
+    <div className="p-4 border border-gray-300 rounded-md bg-white">
+      QnaDetail 페이지
+    </div>
+  );
+}
+
+export default QnaDetail;
