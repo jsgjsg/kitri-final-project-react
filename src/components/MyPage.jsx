0958ada5b9cf6694b34b5a6146f381ff0e757324
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-31 16:37:43 +0900

    Feat: "마이페이지 대강 구현"

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,8 +1,18 @@
 const MyPage = () => {
+
+  // 
   
   return (
-    <div>
+    <div className="p-4 border border-gray-300 rounded-md bg-white">
       MyPage 페이지
+      <br /><br />
+      <div>My 게시글</div>
+      <div>My 댓글</div>
+      <div>My 제품후기</div>
+      <div>My Dr.QnA</div>
+      <div>Logout</div>
+      <div>로그인 정보 수정</div>
+      <div>회원 탈퇴</div>
     </div>
   );
 }
