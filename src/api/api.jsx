2e60ec26f180c0af5a7638968e6efd6d6ed80507
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-08-26 16:15:18 +0900

    Fix: "채팅 기능 복구"

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 // API 기본 URL 설정
 // const API_URL = "http://127.0.0.1:8080/api";
-const API_URL = "http://3.38.115.167:8080/api";
+const API_URL = "http://3.35.176.186:8080/api";
 
 // axios 인스턴스 생성
 const api = axios.create({
--- src/api/publicApi.jsx
+++ src/api/publicApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // API 기본 URL 설정
 // const API_URL = "http://127.0.0.1:8080/api";
-const API_URL = "http://3.38.115.167:8080/api";
+const API_URL = "http://3.35.176.186:8080/api";
 
 const publicApi = axios.create({
   baseURL: API_URL,
