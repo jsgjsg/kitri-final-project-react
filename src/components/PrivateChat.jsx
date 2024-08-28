97c1be839b3b6c8d58ab635b72ae9e250f964072
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-08-28 09:04:33 +0900

    Feat: "로컬로 연결"

--- src/components/PrivateChat.jsx
+++ src/components/PrivateChat.jsx
@@ -62,7 +62,8 @@ function PrivateChat() {
       .catch((error) => console.error("Error fetching chat messages: ", error));
 
     // WebSocket 연결
-    const ws = new WebSocket(`ws://my-first-elb-1013166709.ap-northeast-2.elb.amazonaws.com/chat/${roomId}`); // roomId 기반 연결
+    const ws = new WebSocket(`ws://127.0.0.1:8080/chat/${roomId}`); // roomId 기반 연결
+    // const ws = new WebSocket(`ws://my-first-elb-1013166709.ap-northeast-2.elb.amazonaws.com/chat/${roomId}`); // roomId 기반 연결
     setSocket(ws);
 
     // 서버에서 메시지를 받을 때마다 처리
