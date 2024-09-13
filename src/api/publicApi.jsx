4a11d13b850213f717a4d57285fb7157414e2cce
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-09-13 12:50:45 +0900

    Feat: "ec2에 연결"

--- src/api/publicApi.jsx
+++ src/api/publicApi.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // API 기본 URL 설정
-const API_URL = "http://127.0.0.1:8080/api";
-// const API_URL = "http://3.35.176.186:8080/api";
+// const API_URL = "http://127.0.0.1:8080/api";
+const API_URL = "http://43.203.217.13:8080/api";
 // const API_URL = "http://my-first-elb-1013166709.ap-northeast-2.elb.amazonaws.com/api";
 
 const publicApi = axios.create({
